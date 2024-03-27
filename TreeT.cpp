b759924e328f7549ead06242e9d4a158cecccc11
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2024-03-26 22:31:54 -0700

    Implemented Add method.

--- TreeT.cpp
+++ TreeT.cpp
@@ -1,1 +1,113 @@
 
+#include "TreeT.h"
+
+template<class T>
+TreeT<T>::TreeT() {
+    root = nullptr;
+    numNodes = 0;
+}
+
+template<class T>
+TreeT<T>::~TreeT() {
+
+}
+
+template<class T>
+TreeT &TreeT<T>::operator=(const TreeT &otherTree) {
+//    return <#initializer#>;
+}
+
+template<class T>
+void TreeT<T>::Add(T value) {
+
+    Node* node = new Node;
+    node->value = value;
+
+    Node* curr = root;
+
+    while (curr != nullptr) {
+        if (value > curr->value) {
+            if (curr->right == nullptr) {
+                curr->right = node;
+                break;
+            }
+            curr = curr->right;
+        } else if (value < curr->value) {
+            if (curr->left == nullptr) {
+                curr->left = node;
+                break;
+            }
+            curr = curr->left;
+        } else {
+            delete node;
+            return;
+        }
+    }
+    numNodes ++;
+}
+
+template<class T>
+void TreeT<T>::Remove(T value) {
+
+}
+
+template<class T>
+bool TreeT<T>::Contains(T value) {
+    return false;
+}
+
+template<class T>
+int TreeT<T>::Size() {
+    return 0;
+}
+
+template<class T>
+void TreeT<T>::ResetIterator(Order traverseOrder) {
+
+}
+
+template<class T>
+T TreeT<T>::GetNextItem() {
+    return nullptr;
+}
+
+template<class T>
+void TreeT<T>::DestroyTree(TreeT::Node *node) {
+
+}
+
+template<class T>
+void TreeT<T>::RemoveHelper(TreeT::Node *&subtree, T value) {
+
+}
+
+template<class T>
+void TreeT<T>::DeleteNode(TreeT::Node *&subtree) {
+
+}
+
+template<class T>
+void TreeT<T>::GetPredecessor(TreeT::Node *curr, T &value) {
+
+}
+
+template<class T>
+void TreeT<T>::CopyHelper(TreeT::Node *&thisTree, TreeT::Node *otherTree) {
+
+}
+
+template<class T>
+void TreeT<T>::PlacePreOrder(TreeT::Node *node) {
+
+}
+
+template<class T>
+void TreeT<T>::PlacePostOrder(TreeT::Node *node) {
+
+}
+
+template<class T>
+void TreeT<T>::PlaceInOrder(TreeT::Node *node) {
+
+}
+
